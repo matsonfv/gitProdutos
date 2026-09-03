827f18ffdd855240d547f9b9d006fa2555f0b067
Matson Lima <matson_lima@outlook.com>
2026-09-03 12:01:49 -0300

    pasta front adicionado

---
+++ frontend-produtos/script.js
@@ -0,0 +1,25 @@
+// A URL da nossa API
+const url = "http://localhost:3000/produtos";
+
+/**
+ * 1. BUSCAR PRODUTOS (GET)
+ */
+async function buscarProdutos() {
+   const resposta = await fetch(url);
+   const produtos = await resposta.json();
+   console.log(produtos);
+   
+}
+
+
+/**
+ * 2. SALVAR NOVO PRODUTO (POST)
+ */
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", async function(evento) {
+
+});
+
+// Executa a busca de produtos assim que a página abre
+buscarProdutos();
