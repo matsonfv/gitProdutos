ebae5196edb9677166b9e968bbd158f4f821c2da
Matson <matson_lima@outlook.com>
2026-09-09 21:43:49 -0300

    Adicionado campos de deletar e exibir produtos por id, além de modificações nas funções

--- frontend-produtos/script.js
+++ frontend-produtos/script.js
@@ -4,6 +4,8 @@ const url = "http://localhost:3000/produtos";
 // Referências dos elementos
 const formulario = document.getElementById('formulario');
 const listaProdutos = document.getElementById('lista-produtos');
+const formExibir = document.getElementById('form-exibir');
+const exibirProduto = document.getElementById('exibir-produto');
 
 
 /**
@@ -18,14 +20,19 @@ async function buscarProdutos() {
       const produtos = await resposta.json();
       console.log(produtos);
       
+      listaProdutos.innerHTML = '';
+
       // 3. Itera sobre os dados e cria os cards dinamicamente
       produtos.forEach(produto => {
          const card = document.createElement('div');
          
          card.innerHTML = `
-            <h3>${produto.nome}</h3>
-            <p class="preco">R$ ${Number(produto.preco).toFixed(2)}</p>
-            <p>${produto.descricao}</p>
+            <div class="produto-card">
+               <h3>${produto.nome}</h3>
+               <p class="preco">R$ ${Number(produto.preco).toFixed(2)}</p>
+               <p>${produto.descricao}</p>
+               <button class="btn-excluir" onclick="deletarProduto(${produto.id})">Deletar</button>
+            </div>
             `;
             listaProdutos.appendChild(card);
       });
@@ -36,6 +43,7 @@ async function buscarProdutos() {
    
 };
 
+buscarProdutos(); // Chama a função para buscar os produtos ao carregar a página
 
 /**
  * 2. SALVAR NOVO PRODUTO (POST)
@@ -78,3 +86,73 @@ formulario.addEventListener("submit", async function(evento) {
    }
 
 });
+
+/**
+ * 3. BUSCAR PRODUTOS POR ID (GET)
+ */
+
+async function buscarProdutoPorId() {
+   const id = document.getElementById('id-produto').value;
+
+   try {
+      const resposta = await fetch(`${url}/${id}`);
+      if (!resposta.ok) {
+         alert('Produto não encontrado');
+         throw new Error('Produto não encontrado');
+      }
+      const produtos = await resposta.json();
+      console.log(produtos);
+
+      exibirProduto.innerHTML = '';
+
+      // Atualiza a interface com os detalhes do produto
+      produtos.forEach(produto => {
+         const card = document.createElement('div');
+
+         card.innerHTML = `
+            <div class="produto-card">
+               <h3>${produto.nome}</h3>
+               <p class="preco">R$ ${Number(produto.preco).toFixed(2)}</p>
+               <p>${produto.descricao}</p>
+            </div>
+            `;
+         exibirProduto.appendChild(card);
+      });
+   } catch (erro) {
+      console.error('Erro:', erro);
+   }
+}
+
+formExibir.addEventListener("submit", async function(evento) {
+   // Previne o recarregamento da página
+   evento.preventDefault();
+
+   
+   // Chama a função que busca o produto
+   await buscarProdutoPorId();
+
+   
+});
+
+/**
+ * 4. DELETAR PRODUTOS (DELETE)
+ */
+
+async function deletarProduto(id) {
+   try {
+      const resposta = await fetch(`${url}/${id}`, {
+         method: 'DELETE'
+      });
+      if (!resposta.ok) {
+         throw new Error('Erro ao deletar produto');
+      }
+      console.log('Produto deletado com sucesso :)');
+      alert('Produto deletado com sucesso :)');
+
+      await buscarProdutos();
+      await buscarProdutoPorId();
+      // Aqui você pode atualizar a interface para remover o produto da lista
+   } catch (erro) {
+      console.error('Erro:', erro);
+   }
+}
